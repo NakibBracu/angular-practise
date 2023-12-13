93dbe4313e45f5981fa0374bbca10cc2c1c60d46
Md. Nakib Rahaman <nakibrahamanbracu18@gmail.com>
2023-12-14 00:11:48 +0600

    test.component added!

---
+++ AngularFirstDemo/src/app/TestComponent/test.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'app-test',
+    styleUrl:'./test.component.css',
+    templateUrl:'./test.component.html'
+})
+
+export class TestComponent{
+title = "testComponent";
+num1 = 2;
+num2 = 4;
+}
--- AngularFirstDemo/src/app/app.module.ts
+++ AngularFirstDemo/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './HomeComponent/home.component';
+import { TestComponent } from './TestComponent/test.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    TestComponent
   ],
   imports: [
     BrowserModule,
