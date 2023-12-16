ceec80e883849a2a5ac9d22205046fc3b32bb07f
Nakib Rahaman <nakibrahamanbracu18@gmail.com>
2023-12-17 00:31:41 +0600

    categories component adjusted!

--- AngularRoutingExamples/routingex1/src/app/app.component.ts
+++ AngularRoutingExamples/routingex1/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './Components/home/home.component';
 import { UsersComponent } from './Components/users/users.component';
+import { CategoriesComponent } from './Components/categories/categories.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet,HomeComponent,UsersComponent],
+  imports: [CommonModule, RouterOutlet,HomeComponent,UsersComponent,CategoriesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
