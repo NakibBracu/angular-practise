8262f1495ac84dd6b17e0cdcdedc00cff1beaeac
Nakib Rahaman <nakibrahamanbracu18@gmail.com>
2023-12-17 00:30:32 +0600

    user component adjusted!

--- AngularRoutingExamples/routingex1/src/app/app.component.ts
+++ AngularRoutingExamples/routingex1/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './Components/home/home.component';
+import { UsersComponent } from './Components/users/users.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,HomeComponent,UsersComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
