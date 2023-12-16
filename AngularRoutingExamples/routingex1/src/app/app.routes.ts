b2988e666d74201a6ff46efbc6f3b7b9969efa0f
Nakib Rahaman <nakibrahamanbracu18@gmail.com>
2023-12-17 00:35:36 +0600

    configure routes in app.route.ts!

--- AngularRoutingExamples/routingex1/src/app/app.routes.ts
+++ AngularRoutingExamples/routingex1/src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './Components/home/home.component';
+import { UsersComponent } from './Components/users/users.component';
+import { CategoriesComponent } from './Components/categories/categories.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+{
+    path:'',
+    component:HomeComponent
+},
+{
+    path:'users',
+    component:UsersComponent
+},
+{
+    path:'categories',
+    component:CategoriesComponent
+}
+];
