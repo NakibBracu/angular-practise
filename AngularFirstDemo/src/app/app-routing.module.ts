0cd5727473b2769c9774e4d44fb24a5095e38bfd
Nakib Rahaman <nakibrahamanbracu18@gmail.com>
2023-12-16 16:44:04 +0600

    routes defined in public routing module!

--- AngularFirstDemo/src/app/app-routing.module.ts
+++ AngularFirstDemo/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PublicComponent } from './areas/public/public.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component:PublicComponent,
+    loadChildren:()=>import('./areas/public/public.module').then(m=>m.PublicModule) 
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
