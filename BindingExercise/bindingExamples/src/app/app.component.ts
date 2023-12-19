dbb396a63dd3382745e0719068a9cbb5ec78c6ef
Nakib Rahaman <nakibrahamanbracu18@gmail.com>
2023-12-19 18:48:23 +0600

    FormsModule imported in app.component.ts and form error gone!

--- BindingExercise/bindingExamples/src/app/app.component.ts
+++ BindingExercise/bindingExamples/src/app/app.component.ts
@@ -1,15 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'bindingExamples';
   username = ''
+
+
 }
