5e7f2ff2d2c378ae80c8be3d273e64b47edc2ad0
Nakib Rahaman <nakibrahamanbracu18@gmail.com>
2023-12-20 02:16:37 +0600

    basic calculator implemented!

--- TailwindPractise/TailwindCSSPrac/src/app/app.component.ts
+++ TailwindPractise/TailwindCSSPrac/src/app/app.component.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'TailwindCSSPrac';
+  amount: number;
+  quantity: number;
+  totalPrice: number ;
+  CalculateTotal(event: Event){
+    this.totalPrice = this.amount*this.quantity
+  }
+
 }
